d0d3eceedcdb622e1b4187803d2f80f11c06c76d
noyon-exb <noyon@exabyting.com>
2022-11-30 12:06:49 +0600

    AL-623 initial commit

---
+++ src/TestReactRouter/Page1.js
@@ -0,0 +1,11 @@
+import {Flex, Heading} from "@chakra-ui/react";
+
+const Page1 = () => {
+  return (
+    <Flex width="100%" height="100vh" justify="center" align="center">
+      <Heading>Hello good peoples, Welcome to Page1!</Heading>
+    </Flex>
+  );
+};
+
+export default Page1;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
